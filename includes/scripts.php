74d71be8af16d98f2e30a62e82cf0f5467e58df7
Jonathan Garcia <garciajonathan268@gmail.com>
2026-01-23 14:35:17 -0500

    Se añadio jsqury para los select.

--- includes/scripts.php
+++ includes/scripts.php
@@ -4,6 +4,8 @@
  */
 $root = $root ?? '';
 ?>
+<!-- jQuery (Required for Select2 and other components) -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <!-- Security & Anti-Inspect -->
 <script src="<?php echo $root; ?>assets/js/security.js"></script>
 <!-- Main JS -->
